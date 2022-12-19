7d5dc322cda69b089209446a60b4cb2b98eb6339
WeiLong006 <lim.wei.long.1@gmail.com>
2022-12-19 13:36:18 +0800

    paxNum verification done

--- Start.js
+++ Start.js
@@ -1,5 +1,13 @@
 function changePage() {
-  window.location.href = "https://www.google.com/";
+  const paxNum = document.querySelector("#paxNum").value;
+  if (paxNum > 0) {
+    if (paxNum !== null) {
+      window.location.href = "http://127.0.0.1:5500/Index.html";
+    }
+  } else {
+    window.alert("Please enter a valid number");
+  }
+  console.log(paxNum);
 }
 
 document.querySelector("#start").addEventListener("click", changePage);
