d755d363e9f5e1ecebb21173ba022916515b9d8b
WeiLong006 <lim.wei.long.1@gmail.com>
2022-12-22 11:12:16 +0800

    Arrays changed to images and gradient background included

--- script.js
+++ script.js
@@ -3,48 +3,51 @@ const selected = [];
 
 //Basic Deck
 const cards = [
-  "Give 1",
-  "Give 2",
-  "Take 1",
-  "Take 2",
-  "Swap",
-  "Pick one",
-  "Never have i ever",
-  "Guys drink 1",
-  "Guys drink 2",
-  "Girls drink 1",
-  "Girls drink 2",
+  "/Images/Basic/Give 1.png",
+  "/Images/Basic/Give 2.png",
+  "/Images/Basic/Take 1.png",
+  "/Images/Basic/Take 2.png",
+  "/Images/Basic/Swap.png",
+  "/Images/Basic/Pick one.png",
+  "/Images/Basic/Never have i ever.png",
+  "/Images/Basic/Guys drink 1.png",
+  "/Images/Basic/Guys drink 2.png",
+  "/Images/Basic/Girls drink 1.png",
+  "/Images/Basic/Girls drink 2.png",
 ];
 
 //Rules deck
 const rules = [
-  "Pay compliments",
-  "No swearing",
-  "Swear every time",
-  "That's what she said",
-  "Sing a song",
-  "T-rex arms",
-  "See what see",
-  "Choo Choo",
-  "Drinking buddy",
-  "Remove a rule",
-  "Create a rule",
+  "/Images/Rules/Pay compliments.png",
+  "/Images/Rules/No swearing.png",
+  "/Images/Rules/Swear every time.png",
+  "/Images/Rules/That's what she said.png",
+  "/Images/Rules/Sing a song.png",
+  "/Images/Rules/T-rex arms.png",
+  "/Images/Rules/See what see.png",
+  "/Images/Rules/Choo Choo.png",
+  "/Images/Rules/Drinking buddy.png",
+  "/Images/Rules/Remove a rule.png",
+  "/Images/Rules/Create a rule.png",
 ];
 
 //Naughty deck
 const naughty = [
-  "Hold the hand of the person on your left till your next turn",
-  "Hold the hand of the person on your right till your next turn",
-  "Sit on the lap of the person on your right till your next turn",
-  "Sit on the lap of the person on your left till your next turn",
-  "Give a massage for the person opposite you till your next turn",
-  "Kiss the person on your left",
-  "Kiss the person on your right",
-  "Tell the person on your left how much you left him/her",
-  "Tell the person on your right how much you left him/her",
-  "Whisper a dirty joke to the person on your left, if they don't laugh, you drink",
-  "Whisper a dirty joke to the person on your right, if they don't laugh, you drink",
+  "/Images/Naughty/Hold hands left.png",
+  "/Images/Naughty/Hold hands right.png",
+  "/Images/Naughty/Sit left.png",
+  "/Images/Naughty/Sit right.png",
+  "/Images/Naughty/Massage.png",
+  "/Images/Naughty/Kiss left.png",
+  "/Images/Naughty/Kiss right.png",
+  "/Images/Naughty/Secret Admirer.png",
+  "/Images/Naughty/Shoot Shag Marry.png",
+  "/Images/Naughty/Whisper left.png",
+  "/Images/Naughty/Whisper right.png",
 ];
+
+//Singapore deck
+const Sg = [];
 
 function setCards(numPax, basicSelect, rulesSelect, naughtySelect) {
   // declarations
@@ -56,56 +59,48 @@ function setCards(numPax, basicSelect, rulesSelect, naughtySelect) {
   //creating function for next button, calling the next card after next button is clicked
   const nextButton = document.querySelector("#next");
   nextButton.addEventListener("click", function () {
-    //declarations
+    //randoming the selection
     const random = Math.floor(Math.random() * selected.length);
     const totalCards = Math.floor(Math.random() * 11);
     const drawCard = selected[random][totalCards];
+
+    //declarations for the components
     const display = document.querySelector("#card");
     const countDown = document.querySelector("#countDown");
     const mainImg = document.querySelector("#mainImg");
-    if (clickCount < pax) {
-      if (basicSelect) {
-        if (random === 0) {
-          //Display card
-          mainImg.src = `/Images/0/${drawCard}.png`;
-          display.appendChild(mainImg);
-        }
-        if (random === 1) {
-          //Display card
-          mainImg.src = `/Images/1/${drawCard}.png`;
-          display.appendChild(mainImg);
-        }
-        // if (random === 2) {
-        //   //Display card
-        //   mainImg.src = `/Images/2/${drawCard}.png`;
-        //   display.appendChild(mainImg);
-        // }
-      } else if (rulesSelect) {
-        if (random === 0) {
-          //Display card
-          mainImg.src = `/Images/1/${drawCard}.png`;
-          display.appendChild(mainImg);
-        }
-        // if (random === 1){
-        // //Display card
-        // mainImg.src = `/Images/2/${drawCard}.png`;
-        // display.appendChild(mainImg);
-        // }
-      }
-    }
 
-    //count down for both cards left and click count. Display cound down
+    //display cards
+    document.querySelector("#header2").style.display = "none";
+    document.querySelector("#countDownLabel").style.display = "block";
+    countDown.style.display = "block";
+    mainImg.src = drawCard;
+    display.appendChild(mainImg);
+
+    //count down for both cards left and click count. Display count down
     clickCount += 1;
     countPax -= 1;
     countDown.innerHTML = `${countPax}`;
 
-    // if click count hits max number of cards
+    //Change background color dependent on deck
+    if (drawCard.includes("Basic")) {
+      document.body.style.backgroundColor = "green";
+    }
+
+    if (drawCard.includes("Rules")) {
+      document.body.style.backgroundColor = "black";
+    }
+
+    if (drawCard.includes("Naughty")) {
+      document.body.style.backgroundColor = "maroon";
+    }
+
+    // if click count hits max number of cards to display winner image
     if (clickCount >= pax) {
-      // const img = document.createElement("img");
       const win = "Winner";
       mainImg.src = `/Images/${win}.png`;
-      // img.src = "/Images/Winner.png";
       display.appendChild(mainImg);
+      document.body.style.backgroundClip = "black";
+      nextButton.innerText = "END!        ";
 
       // reload page and click count when player clicks next after the max cards have been played
       if (clickCount > pax) {
@@ -119,8 +114,10 @@ function setCards(numPax, basicSelect, rulesSelect, naughtySelect) {
 // Get the modal
 const modal = document.querySelector("#myModal");
 
-// Start Button
+// Start Button and header to disappear
 const start = document.querySelector("#start");
+const head = document.querySelector("#header");
+const startPic = document.querySelector("#startPic");
 
 // Submit button
 const submit = document.querySelector("#submit");
@@ -134,7 +131,7 @@ start.addEventListener("click", function () {
 submit.addEventListener("click", function () {
   const paxNum = document.querySelector("#paxNum").value;
 
-  //check if the input box value is valid
+  //check if the input box value is valid and push the selected categories
   let basicSelect = false;
   let rulesSelect = false;
   let naughtySelect = false;
@@ -155,6 +152,8 @@ submit.addEventListener("click", function () {
       modal.style.display = "none";
       document.querySelector(".game").style.display = "block";
       start.style.display = "none";
+      head.style.display = "none";
+      startPic.style.display = "none";
       setCards(paxNum, basicSelect, rulesSelect, naughtySelect);
       console.log(selected);
     }
